b56509f628d2eb614fe3e3e8f5d5a4dd2ec63a5e
sss <kirillku4mar24@gmail.com>
2021-04-01 14:25:37 +0300

    Initial Commit

---
+++ jsndsnl/jsndsnlApp.swift
@@ -0,0 +1,17 @@
+//
+//  jsndsnlApp.swift
+//  jsndsnl
+//
+//  Created by Кирилл Кучмар on 01.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct jsndsnlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
